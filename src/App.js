27b3c6eef7b75fd7d980eee77e4a2a0bbec146e2
Lailz <lailaabdulraheem@gmail.com>
2020-06-29 16:41:19 +0300

    added map for cookies

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
 
-const styles = {
-  cookie: {
-    margin: "20px",
-  },
-  cookieImage: {
-    width: "200px",
-    height: "200px",
-  },
-  list: {
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-  },
-  shopImage: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "50%",
-  },
-  text: {
-    textAlign: "center",
-  },
-};
+// Data
+import cookies from "./cookies";
+
+// Styles
+import styles from "./styles";
 
 function App() {
+  console.log(cookies[0].name);
+  const example = () => {
+    if (false)
+      return (
+        <div>
+          <h1>HI I'm LUIS</h1>
+          <h1>I HAVE A QUESTION</h1>
+        </div>
+      );
+  };
+
+  const cookieList = cookies.map((cookie) => (
+    <div style={styles.cookie} key={cookie.id}>
+      <img src={cookie.image} alt={cookie.name} style={styles.cookieImage} />
+      <p style={styles.text}>{cookie.name}</p>
+      <p style={styles.text}>{cookie.price} KD</p>
+    </div>
+  ));
+
   return (
     <div>
       <div>
+        {example()}
         <h1 style={styles.text}>Cookies and Beyond</h1>
         <h4 style={styles.text}>Where cookie maniacs gather</h4>
         <img
@@ -36,26 +38,7 @@ function App() {
           style={styles.shopImage}
         />
       </div>
-      <div style={styles.list}>
-        <div style={styles.cookie}>
-          <img
-            src="https://joyfoodsunshine.com/wp-content/uploads/2016/01/best-chocolate-chip-cookies-recipe-ever-no-chilling-1.jpg"
-            alt="chocolate chip cookies"
-            style={styles.cookieImage}
-          />
-          <p style={styles.text}>Chocolate Chip Cookies</p>
-          <p style={styles.text}>20 KD</p>
-        </div>
-        <div style={styles.cookie}>
-          <img
-            src="https://i.pinimg.com/originals/f6/3e/2a/f63e2a1cd0c7d3c0ab9cd277d3f32050.jpg"
-            alt="cute cookies"
-            style={styles.cookieImage}
-          />
-          <p style={styles.text}>Cute Cookies</p>
-          <p style={styles.text}>25 KD</p>
-        </div>
-      </div>
+      <div style={styles.list}>{cookieList}</div>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,25 @@
+const styles = {
+  cookie: {
+    margin: "20px",
+  },
+  cookieImage: {
+    width: "200px",
+    height: "200px",
+  },
+  list: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  shopImage: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50%",
+  },
+  text: {
+    textAlign: "center",
+  },
+};
+
+export default styles;
