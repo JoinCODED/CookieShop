a2ebddf09b0aa3b497e1358236b1aad16b071e8a
Lailz <lailaabdulraheem@gmail.com>
2020-07-12 18:38:16 +0300

    add home

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Styles
+import { Description, ShopImage, Title } from "../styles";
+
+const Home = () => {
+  return (
+    <>
+      <Title>Cookies & Beyond</Title>
+      <Description>Where cookie maniacs gather</Description>
+      <ShopImage
+        src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+        alt="cookie shop"
+      />
+    </>
+  );
+};
+
+export default Home;
