28056ce46ea0f839685c7f024be05b3f93055316
Lailz <lailaabdulraheem@gmail.com>
2020-07-12 18:37:32 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router";
 
 // Components
+import CookieDetail from "./components/CookieDetail";
 import CookieList from "./components/CookieList";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+
+// Data
+import cookies from "./cookies";
 
 // Styles
-import styles from "./styles";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles";
+
+const theme = {
+  light: {
+    mainColor: "#242424",
+    backgroundColor: "#fefafb",
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+  dark: {
+    mainColor: "#fefafb",
+    backgroundColor: "#242424",
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+};
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+  const [_cookies, setCookies] = useState(cookies);
+
+  const createCookie = (newCookie) => {
+    console.log("createCookie -> newCookie", newCookie);
+
+    const updatedCookies = [..._cookies, newCookie];
+    setCookies(updatedCookies);
+  };
+
+  const deleteCookie = (cookieId) => {
+    const updatedCookies = _cookies.filter((cookie) => cookie.id !== cookieId);
+    setCookies(updatedCookies);
+  };
+
+  const handleToggle = () =>
+    setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+
   return (
-    <div>
-      <div>
-        <h1 style={styles.text}>Cookies and Beyond</h1>
-        <h4 style={styles.text}>Where cookie maniacs gather</h4>
-        <img
-          src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
-          alt="cookie shop"
-          style={styles.shopImage}
-        />
-      </div>
-      <CookieList />
-    </div>
+    <ThemeProvider theme={theme[currentTheme]}>
+      <GlobalStyle />
+      <NavBar currentTheme={currentTheme} handleToggle={handleToggle} />
+      <Switch>
+        <Route path="/cookies/:cookieSlug">
+          <CookieDetail cookies={_cookies} deleteCookie={deleteCookie} />
+        </Route>
+        <Route path="/cookies">
+          <CookieList
+            cookies={_cookies}
+            createCookie={createCookie}
+            deleteCookie={deleteCookie}
+          />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
