2e6f62585e20bf94898a6e89e1c82a0c0c0bf0e7
Lailz <lailaabdulraheem@gmail.com>
2021-12-12 19:34:36 +0300

    change api

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:8000",
-  baseURL: "https://cookie-shop-test.herokuapp.com",
+  baseURL: "http://localhost:8000",
+  // baseURL: "https://cookie-shop-test.herokuapp.com",
 });
 
 export default instance;
